5822c7e7620540fe5ef959d01d926a1992cd0286
Pac00n <59127096+Pac00n@users.noreply.github.com>
2025-06-04 23:22:23 +0100

    Configure ESLint and upgrade fal.ai client

---
+++ api/eslint.config.js
@@ -0,0 +1,18 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {},
+  },
+];
---
+++ web/eslint.config.js
@@ -0,0 +1,11 @@
+import next from 'eslint-plugin-next';
+
+export default [
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      next,
+    },
+    rules: {},
+  },
+];
